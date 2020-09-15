29c75566c87cd01c83a04fcd9c135ccc67ef4cb0
João Vitor de Aguiar Debossan <jvdebossan@gmail.com>
2020-09-15 14:17:43 -0300

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,239 @@
+lista_iniciar = [
+    '***Sistema inicializado. Conexão com o banco de dados estabelecida :globe_with_meridians:***',
+    '***Conectando ao servidor remoto... :globe_with_meridians:***',
+    '***Ip 173.274.27.01 acabou de estabelecer uma conexão :globe_with_meridians:***',
+    '***Ligando servidores internos... ativando mainframe... :globe_with_meridians:***',
+    '***Carregando banco de dados remoto. Iniciando protocolo de login :globe_with_meridians:***',
+    '***Conectando ao IP 163.274.247.01... Conexão estabelecida :globe_with_meridians:***',
+    '***Conectado com sucesso :globe_with_meridians:***',
+    '***Sistema inicializado. Conectando ao Banco de dados :globe_with_meridians:***'
+]
+
+lista_gif_berti = [
+    'https://tenor.com/view/berti-sarrador-dancing-hyped-summer-pool-gif-17763539',
+    'https://tenor.com/view/bruno-berti-irmaos-berti-irm%c3%a3os-berti-lucas-berti-porrada-gif-17518204',
+    'https://tenor.com/view/berti-bruno-berti-bertinho-berte-berti-dancing-thrusting-gif-17771441',
+    'https://tenor.com/view/bruno-berti-lucas-berti-irmaos-berti-indias-brega-funk-gif-16727590',
+    'https://tenor.com/view/bruno-berti-lucas-berti-irmaos-berti-indias-brega-funk-gif-16727590',
+    'https://tenor.com/view/berti-dark-sarrada-berti-dark-berti-sarrada-gif-17764030',
+    'https://tenor.com/view/sarrando-berti-dancing-humping-gif-17763559',
+    'https://tenor.com/view/sarrada-berti-dancing-gif-17895234',
+    'https://media.discordapp.net/attachments/643918032468443177/733444131711352913/bertinho.gif?'
+]
+
+lista_gif_gostosas3d = [
+    'https://tenor.com/view/sexy-jordana-brewster-bikini-pictorial-gif-13847158',
+    'https://giphy.com/gifs/twerk-gnE9JR4OxfLig',
+    'https://giphy.com/gifs/things-bounce-thechive-SInIQtCeyj1C0',
+    'https://giphy.com/gifs/notorious-big-t1kRtJmCI4R1e',
+    'https://giphy.com/gifs/a-mans-butt-tight-butts-sexy-guy-3o85xpi3wbmElPKSFq',
+    'https://giphy.com/gifs/10x81OqDjnQhr2',
+    'https://giphy.com/gifs/funkofficial-hand-3ohzdIfd39bRZhKRLa',
+    'https://giphy.com/gifs/things-bounce-thursday-HbeZElJ8EjrEY',
+    'https://giphy.com/gifs/dance-sfw-pov-11bNExgnwGHBuw',
+    'https://giphy.com/gifs/the-rock-vince-mcmahon-rikishi-127h8dMHnk5H5C',
+    'https://tenor.com/view/mia-khalifa-lip-bite-gif-13295397'
+]
+
+lista_gif_gostosas2d = [    
+    'https://images-ext-2.discordapp.net/external/pEirYOB-Aw3qjC-hwhLJPNSCqw1_Qt9TtBD5JFb1Sd8/https/imgur.com/DrjW7Bk.gif',
+    'https://tenor.com/view/yosuga-no-sora-sora-anime-good-girl-beautiful-gif-16500791',
+    'https://tenor.com/view/anime-rias-gremory-undressed-manga-gif-16310335',
+    'https://tenor.com/view/anime-life-love-gif-13861593',
+    'https://tenor.com/view/highschooldxd-rias-akeno-koneko-bouncing-gif-6206479',
+    'https://tenor.com/view/yandere-knife-cute-anime-gif-14593966',
+    'https://tenor.com/view/iam4ming-sex-gif-9390805',
+    'https://tenor.com/view/fortnite-dance-minecraft-steve-dance-steve-fortnite-steve-gif-15531052',
+    'https://images-ext-1.discordapp.net/external/swaC2EQsPJaMgnCusSW4Gd85MT7qFPXg8SIDrv6_lIY/https/imgur.com/pv2wKTc.gif',
+    'https://tenor.com/view/cute-anime-dancing-silly-happy-excited-gif-13462237',
+    'https://tenor.com/view/anime-dance-happy-catgirl-gif-9765182',
+    'https://tenor.com/view/sasuke-anime-naruto-dance-xd-gif-8074573'
+] 
+
+lista_gif_adm = [
+    'https://giphy.com/gifs/dvOwFmfbzmAsI9v2IV',
+    'https://tenor.com/view/will-smith-banido-meme-dance-gif-16971203',
+    'https://tenor.com/view/teste-adm-gif-18233312',
+    'https://tenor.com/view/banido-adm-administrador-ban-adm-on-gif-16662125',
+    'https://tenor.com/view/depressor-adm-n%c3%a3o-me-da-ban-depressao-n%c3%a3o-d%c3%a1ban-adm-adm-gif-16714385',
+    'https://tenor.com/view/wmiguel17-banido-sasuke-jutsu-banned-gif-17769467',
+    'https://tenor.com/view/adm-ban-regra-gif-18049769',
+    'https://tenor.com/view/treinando-pra-ser-adm-gif-18025277',
+    'https://tenor.com/view/treinando-pra-ser-adm-gif-18025277',
+    'https://tenor.com/view/atm-parado-is-taking-abath-carabao-bathing-river-carabao-bathing-gif-17779749',
+    'https://tenor.com/view/adm-eu-nao-li-as-regras-dance-gif-17880739',
+    'https://tenor.com/view/banido-thor-adm-administrador-chris-hemsworth-gif-17550051'
+]
+
+lista_gif_paitaon = [
+    'https://tenor.com/view/pai-ta-on-big-chungus-reddit-arevalo-dance-gif-16955335',
+    'https://tenor.com/view/pai-ta-on-dancing-clown-grooves-dance-moves-gif-17049247',
+    'https://tenor.com/view/pai-ta-on-clown-dancing-gif-17553708',
+    'https://tenor.com/view/pai-ta-on-dancing-groovy-jumping-the-mask-gif-17772245'
+    
+]
+
+lista_gif_paitaoff = [
+    'https://tenor.com/view/pai-ta-off-maskara-pai-ta-on-pai-ta-gif-18143937',
+    'https://tenor.com/view/pai-ta-chato-weekend-vibe-mood-gif-13849068'
+    'https://tenor.com/view/paitaoff-gif-17983669'
+]
+
+lista_gif_furros = [
+    'https://tenor.com/view/furry-gif-4903957',
+    'https://tenor.com/view/furry-furries-fa-ba-fantasy-basel-dance-gif-15180558',
+    'https://tenor.com/view/dance-furry-cute-dance-move-gif-16336211',
+    'https://tenor.com/view/game-grumps-furry-fursuit-gif-13526097',
+    'https://tenor.com/view/mlp-heart-love-gif-9643704',
+    'https://tenor.com/view/legoshi-beastars-furry-nervous-gif-15338729',
+    'https://tenor.com/view/furry-snug-furry-furry-love-sweet-gif-17325139',
+    'https://tenor.com/view/furry-squad-sniper-job-autumnfallings-gif-7485644',
+    'https://tenor.com/view/furry-gif-11210434'
+]
+
+lista_gif_patos = [
+    'https://tenor.com/view/duck-run-running-chase-come-back-here-gif-4536793',
+    'https://tenor.com/view/gary-tay-kitten-duckling-gif-7517574',
+    'https://tenor.com/view/duck-quack-walk-gif-9046918',
+    'https://tenor.com/view/cute-duck-cuddle-its-ok-adorable-duck-gif-12985802',
+    'https://tenor.com/view/sleepy-duck-gif-4521800',
+    'https://tenor.com/view/aflack-dancing-duck-gif-10526585',
+    'https://giphy.com/gifs/dancing-duck-aflack-b9QBHfcNpvqDK',
+    'https://giphy.com/gifs/duck-2ATElMHGKoVeo',
+    'https://giphy.com/gifs/animated-3d-animal-Y0zTJ7VrKo9P2',
+    'https://giphy.com/gifs/duck-cuteness-baby-82nxC1u2BC8VU1wiZq'
+]
+
+lista_gif_kawai = [
+    'https://tenor.com/view/cute-adorable-charming-gif-13798021',
+    'https://tenor.com/view/japan-anime-kawaii-sugoi-gif-4761309',
+    'https://tenor.com/view/milk-and-mocha-hugs-bear-couple-love-cute-gif-12498539',
+    'https://tenor.com/view/excited-anime-cute-gif-5967731',
+    'https://tenor.com/view/kawaii-kokoro-love-loli-gif-9484577',
+    'https://tenor.com/view/loli-police-fbi-anime-sit-gif-16156795',
+    'https://tenor.com/view/chloevoneinzbern-fate-fatekalied-loli-anime-gif-9809880',
+    'https://tenor.com/view/loli-hearts-love-in-love-cute-gif-17645914',
+    'https://tenor.com/view/cute-kawaii-zomg-gif-12496012',
+    'https://tenor.com/view/hello-guys-cute-kawaii-happy-gif-13759559',
+    'https://tenor.com/view/kawaii-cute-yui-gif-13400599'
+]
+
+lista_salves = [
+    'Salve fi, pai ta on.',
+    'Cheguei.',
+    'To aqui man.',
+    'Salve garai bagulho é loko memo.',
+    'Baguio é rardicobra.',
+    'Salve salve, o dono do server é mó gado.',
+    'Salve, sabia que o Biel é o ser mais lindo do universo?',
+    'Salve, amante do deus zoio.',
+    'Sé loko mano, salve kebrada.',
+    'Chamou?',
+    'Salve :)',
+    'Salve, lembre-se de se manter hidratado.',
+]
+
+lista_gif_bolsonaro = [
+    'https://tenor.com/view/bolsonaro-2018-gif-10813661',
+    'https://tenor.com/view/bolsonaro-face-mask-wave-heart-gif-17590213',
+    'https://tenor.com/view/bolsonaroarminha-bolsonaro-bolsonaroempolgado-bang-bang-bang-gif-12433792',
+    'https://tenor.com/view/bolsonaro-camera-camera-stand-gif-12770014',
+    'https://tenor.com/view/grande-dia-bolsonaro-presidente-brasil-thumbs-up-gif-14439617',
+    'https://tenor.com/view/bolsonaro-nando-moura-gif-8680576',
+    'https://tenor.com/view/felipe-neto-amabol-sonaro-tattoo-laugh-felipe-neto-vlogger-gif-17178756',
+    'https://giphy.com/gifs/gptv-trump-donald-bolsonaro-hr3AZuH7Y8Dvba9jUf',
+    'https://giphy.com/gifs/programapanico-mito-bolsonabo-3o6nUT58nRNMIM0umQ',
+    'https://media.discordapp.net/attachments/748691279352823979/748695801315983380/19b5rrcyuqu41.png'
+]
+
+lista_frases = [
+    '"Liberdade sem leis significa anarquia; leis sem liberdade significa tirania." - Tanya Degurechaff',
+    '"Milagres são ilusões causadas por observação e entendimento insuficientes. Eles são apenas... gloriosos mal-entendidos." - Tanya Degurechaff',
+    '"Qualquer coisa que pode dar errado, vai dar errado. Essa é uma lei famosa. Se você viver uma vida longa, entenderá." - Tanya Degurechaff',
+    '"Não importa o quanto nos modernizamos, não importa como as normas sociais nos afetam, os seres humanos são criaturas tolas." - Tanya Degurechaff',
+    '"Vitória. Uma coisa tão tentadora. É claro que todo mundo quer apreciar seu sabor." - Tanya Degurechaff',
+    '"O exército é uma organização. E as organizações não são nada sem regras!" - Tanya Degurechaff',
+    '"O racionalismo não é a única coisa que move as pessoas." - Tanya Degurechaff',
+    '"Honestamente, eu odeio a guerra. Pessoas matando umas as outras. Isso é a pior coisa que a humanidade poderia ter criado. Um desperdício de humanos e economias. No entanto, nossos inimigos são comunistas. Totalitários violando a liberdade pessoal. Não posso viver debaixo do mesmo teto que os comunistas. Temos de pegar em armas, para podermos viver uma vida feliz." - Tanya Degurechaff',
+    '"Senhor, ouçais a minha prece é realizai o meu desejo. Do céu,trazei a baixo o martelo dá vossa irá." - Tanya Degurechaff',
+    '"Tudo que é feito de carne é como se fosse mato, e toda a gloria dos homens é como se fossem flores campestres. O mato seca, as flores caem.Mas as palavras do Senhor Estarão sempre presentes." - Alucard',
+    '"Quando as lágrimas de um ser humano enfim secam para sempre...Este se tornar um demônio,um monstro... Então ri...Ri com teu orgulho e tua arrogância...como sempre fez..." - Alucard',
+    '"Não importa em que época estejamos vivendo , sempre haverá a falta de originalidade." - Alucard',
+    '"Os humanos são os únicos seres que se divertem com o sofrimento dos seus semelhantes." - Alucard',
+    '"A perseverança de quem está no comando incendeia a alma de quem o segue." - Alucard',
+    '"Somente humanos podem matar monstros!" - Alucard',
+    '"Os computadores são incrivelmente rápidos, precisos e burros; os homens são incrivelmente lentos, imprecisos e brilhantes; juntos, seus poderes ultrapassam os limites da imaginação." - Albert Einstein'
+]
+
+lista_gif_tapas = [
+    'https://media.tenor.com/images/612e257ab87f30568a9449998d978a22/tenor.gif',
+    'https://media.tenor.com/images/fe6bba40e429523927bd033001a7b4f1/tenor.gif',
+    'https://media.tenor.com/images/d81ff8332c2138d4306617751e792cbc/tenor.gif',
+    'https://media.tenor.com/images/3fd96f4dcba48de453f2ab3acd657b53/tenor.gif',
+    'https://media.tenor.com/images/153b2f1bfd3c595c920ce60f1553c5f7/tenor.gif',
+    'https://media.tenor.com/images/a1c3dfd707f83075511efa5de3ddf2c8/tenor.gif',
+    'https://media.tenor.com/images/6c0931d51f07ff4ae892fb4732173591/tenor.gif',
+    'https://media.tenor.com/images/2843b47b224aacbb3d09b06cfc5c4af9/tenor.gif',
+    'https://media.tenor.com/images/68c04dca2d4aef54dd3057401db83320/tenor.gif',
+    'https://media.tenor.com/images/0924d7d4023690de91eabd1d3e02ea75/tenor.gif',
+    'https://media.tenor.com/images/f2e22829f9dc2e796d8e9d0590e8076c/tenor.gif',
+    'https://media.tenor.com/images/abc1263c66f3a311eeb923bd7c281087/tenor.gif',
+    'https://media.tenor.com/images/6755279be89384f3997f75f1b6d15a61/tenor.gif',
+    'https://media.tenor.com/images/f481a1ff553336b2e713d0080a270ff6/tenor.gif',
+    'https://media.tenor.com/images/24835679fcb92604851c4be3087dd949/tenor.gif',
+    'https://media.tenor.com/images/3ee6c279dd97a1e97409a54dd90a1972/tenor.gif',
+    'https://media.tenor.com/images/f12ea58221df986b9d62eb1a95ee96e1/tenor.gif',
+    'https://media.tenor.com/images/8b34dfa9d06e700b5059b6fe943ffd14/tenor.gif'
+]
+
+lista_gif_xandao = [
+    'https://tenor.com/view/xandao-xand%C3%A3o-doublebiceps-biceps-gif-18306463',
+    'https://tenor.com/view/xand%C3%A2o-gif-18373720',
+    'https://tenor.com/view/xand%C3%A3o-gif-18434365',
+    'https://cdn.discordapp.com/attachments/755084796782182480/755087115976704071/1dd33d0c-fb02-400d-b1cd-ce5ecf4c23d8-profile_image-150x150.png',
+    'https://cdn.discordapp.com/attachments/755084796782182480/755087174914932848/xandao-o-heroi-dos-herois-20431328-080920200841.png'
+]
+
+lista_gif_atirar = [
+    'https://tenor.com/view/kermit-shoot-lol-gun-frog-gif-16181496',
+    'https://tenor.com/view/kermit-shoot-lol-gun-frog-gif-16181496',
+    'https://tenor.com/view/gun-shoot-shooting-shooting-range-gif-11257865',
+    'https://tenor.com/view/shoot-fish-machine-gun-rifle-river-pescas-gif-12698559',
+    'https://tenor.com/view/cat-shooting-mouth-open-gif-15017033',
+    'https://tenor.com/view/assasination-classrom-gun-shooting-anime-gif-12494509',
+    'https://tenor.com/view/cartoon-machine-gun-%e0%b8%96%e0%b8%a5%e0%b9%88%e0%b8%a1-shooting-gun-shot-gif-11536483',
+    'https://tenor.com/view/fail-gun-shooting-gif-13083383',
+    'https://tenor.com/view/thug-life-thug-teeth-brushing-tooth-brush-gun-gif-4097029',
+    'https://tenor.com/view/shooting-firing-bulls-eye-poin-aiming-gif-13006510',
+    'https://tenor.com/view/atirando-um-sapato-tiago-cadore-professor-bravo-irritado-bravo-gif-16623001',
+    'https://tenor.com/view/shoot-gift-gif-13384546',
+    'https://tenor.com/view/family-guy-ripaim-random-fire-out-of-control-fire-gif-5980510',
+    'https://tenor.com/view/shoot-turkey-gif-11498173'
+]
+
+lista_gif_suicidio = [
+    'https://tenor.com/view/pew-shootme-dead-gif-4355914',
+    'https://tenor.com/view/how-imet-your-mother-robin-scherbatsky-cobie-smulders-neil-patrick-harris-barney-stinson-gif-4317122',
+    'https://tenor.com/view/kermit-suicide-hair-flip-gif-5140737',
+    'https://tenor.com/view/finger-gun-barney-himym-nph-neil-patrick-harris-gif-4524247',
+    'https://tenor.com/view/king-of-the-hill-hank-hank-hill-water-hose-shoot-me-gif-5206956',
+    'https://tenor.com/view/suicide-gif-14427950',
+    'https://tenor.com/view/suicide-bathroom-girl-suicide-false-suicide-bathroom-gif-13342623'
+]
+
+lista_gif_beijar = [
+    'https://tenor.com/view/kiss-couple-love-sweet-gif-16490903',
+    'https://tenor.com/view/kissing-gif-7669855',
+    'https://tenor.com/view/kisses-love-cartoon-cat-gif-10194764',
+    'https://tenor.com/view/agents-of-sheild-kiss-kissing-gif-4753737',
+    'https://tenor.com/view/kiss-anime-love-couple-gif-14240425',
+    'https://tenor.com/view/anime-kissing-kiss-love-gif-10356314',
+    'https://tenor.com/view/first-kiss-naruhina-naruto-hinata-shippuden-gif-13820874',
+    'https://tenor.com/view/love-kiss-kissing-gif-13311446',
+    'https://tenor.com/view/hakuoki-hakuouki-kiss-chizuru-yukimura-chizuru-gif-5262571',
+    'https://tenor.com/view/love-anime-kiss-hot-damn-gif-9838409',
+    'https://tenor.com/view/cat-cats-kissing-gif-9499682',
+    'https://tenor.com/view/golden-time-anime-kiss-couple-lovers-gif-6155670',
+    'https://tenor.com/view/kissing-accidentalkiss-sasuke-sasukeuchiha-uchihasasuke-gif-5515355',
+    'https://tenor.com/view/anime-kiss-love-sweet-gif-5095865'
+]
